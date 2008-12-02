899e3c85465085741bdb22fba0b5e0aa6507285a
Marcus Ramberg <mramberg@cpan.org>
2008-12-02 12:38:57 +0000

    Import of MRAMBERG/Catalyst-Plugin-Geography-0.02 from CPAN.
    gitpan-cpan-distribution: Catalyst-Plugin-Geography
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         MRAMBERG/Catalyst-Plugin-Geography-0.02.tar.gz
    gitpan-cpan-author:       MRAMBERG
    gitpan-cpan-maturity:     released

--- lib/Catalyst/Plugin/Geography.pm
+++ lib/Catalyst/Plugin/Geography.pm
@@ -3,7 +3,7 @@ package Catalyst::Plugin::Geography;
 use strict;
 use Catalyst::Plugin::Geography::Implementation;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 *geo = \&geography;    # Makes sri happy ;)
 
